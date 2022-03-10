c4ee8618a518b1b40f18c5e5ccbfa5e27e09a7d9
Tsubasa Sasaki <tsubasa.sasaki.sys21@morijyobi.ac.jp>
2022-03-10 12:38:30 +0900

    javaファイルに変更

---
+++ pra2.java
@@ -0,0 +1,10 @@
+public class pra2{
+	public static void main(String[]args){
+        int i=0;
+        while(i<100){
+        system.out.println("Hello World");
+        i++;
+        }
+        }
+    }
+
